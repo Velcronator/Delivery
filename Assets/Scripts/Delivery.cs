8909b0546105d5e6243adeae73b6fbd02e369952
velcronator <valacos7@gmail.com>
2023-02-23 22:46:10 +1100

    Sprite colour Change

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -1,35 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Delivery : MonoBehaviour
 {
     bool hasPackage = false;
-    [SerializeField] float destroyDelay = 1f;
+    [SerializeField] float destroyDelay = 3f;
+    [SerializeField] Color32 hasPackageColor = new Color32(0, 255, 35, 255);
+    [SerializeField] Color32 noPackageColor = new Color32(255, 255, 255, 255);
+    private SpriteRenderer spriteRenderer;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("collision a " + collision.gameObject);
+        //Debug.Log("collision a " + collision.gameObject);
+    }
+
+    private void Start()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Alien")
         {
-            Debug.Log("Trigger a " + collision.gameObject);
             if (!hasPackage)
             {
                 SceneManager.LoadScene(0);
             }
+
+            Animator anim = collision.gameObject.GetComponentInChildren<Animator>();
+            anim.SetTrigger("Die");
+            Destroy(collision.gameObject, destroyDelay);
+            spriteRenderer.color = noPackageColor;
             hasPackage = false;
         }
 
         if (collision.tag == "Package" && !hasPackage)
         {
             Destroy(collision.gameObject, destroyDelay);
+            spriteRenderer.color = hasPackageColor;
             hasPackage = true;
-            Debug.Log("Trigger a " + collision.gameObject);
         }
 
     }
